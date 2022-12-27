b0af71a9291080a0a56e5847ce0d0e3a0cae852a
Vincent Tremblay <vincent@vtremblay.dev>
2022-12-26 21:35:48 -0500

    spidev: Add Silicon Labs EM3581 device compatible
    [ Upstream commit c67d90e058550403a3e6f9b05bfcdcfa12b1815c ]
    
    Add compatible string for Silicon Labs EM3581 device.
    
    Signed-off-by: Vincent Tremblay <vincent@vtremblay.dev>
    Link: https://lore.kernel.org/r/20221227023550.569547-2-vincent@vtremblay.dev
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Stable-dep-of: fc28d1c1fe3b ("spi: spidev: add correct compatible for Rohm BH2228FV")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -701,6 +701,7 @@ static const struct spi_device_id spidev_spi_ids[] = {
 	{ .name = "spi-petra" },
 	{ .name = "spi-authenta" },
 	{ .name = "spidev" },
+	{ .name = "em3581" },
 	{},
 };
 MODULE_DEVICE_TABLE(spi, spidev_spi_ids);
@@ -728,6 +729,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "cisco,spi-petra", .data = &spidev_of_check },
 	{ .compatible = "micron,spi-authenta", .data = &spidev_of_check },
 	{ .compatible = "rockchip,spidev", .data = &spidev_of_check },
+	{ .compatible = "silabs,em3581", .data = &spidev_of_check },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
