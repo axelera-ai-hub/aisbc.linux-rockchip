c05c2fecb131118485fde8f9a1d0e2db5a973909
Selvarasu Ganesan <selvarasu.g@samsung.com>
2025-08-08 18:23:05 +0530

    usb: dwc3: Remove WARN_ON for device endpoint command timeouts
    commit 45eae113dccaf8e502090ecf5b3d9e9b805add6f upstream.
    
    This commit addresses a rarely observed endpoint command timeout
    which causes kernel panic due to warn when 'panic_on_warn' is enabled
    and unnecessary call trace prints when 'panic_on_warn' is disabled.
    It is seen during fast software-controlled connect/disconnect testcases.
    The following is one such endpoint command timeout that we observed:
    
    1. Connect
       =======
    ->dwc3_thread_interrupt
     ->dwc3_ep0_interrupt
      ->configfs_composite_setup
       ->composite_setup
        ->usb_ep_queue
         ->dwc3_gadget_ep0_queue
          ->__dwc3_gadget_ep0_queue
           ->__dwc3_ep0_do_control_data
            ->dwc3_send_gadget_ep_cmd
    
    2. Disconnect
       ==========
    ->dwc3_thread_interrupt
     ->dwc3_gadget_disconnect_interrupt
      ->dwc3_ep0_reset_state
       ->dwc3_ep0_end_control_data
        ->dwc3_send_gadget_ep_cmd
    
    In the issue scenario, in Exynos platforms, we observed that control
    transfers for the previous connect have not yet been completed and end
    transfer command sent as a part of the disconnect sequence and
    processing of USB_ENDPOINT_HALT feature request from the host timeout.
    This maybe an expected scenario since the controller is processing EP
    commands sent as a part of the previous connect. It maybe better to
    remove WARN_ON in all places where device endpoint commands are sent to
    avoid unnecessary kernel panic due to warn.
    
    Cc: stable <stable@kernel.org>
    Co-developed-by: Akash M <akash.m5@samsung.com>
    Signed-off-by: Akash M <akash.m5@samsung.com>
    Signed-off-by: Selvarasu Ganesan <selvarasu.g@samsung.com>
    Acked-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://lore.kernel.org/r/20250808125315.1607-1-selvarasu.g@samsung.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1865,7 +1865,11 @@ static int __dwc3_stop_active_transfer(struct dwc3_ep *dep, bool force, bool int
 		dep->flags |= DWC3_EP_DELAY_STOP;
 		return 0;
 	}
-	WARN_ON_ONCE(ret);
+
+	if (ret)
+		dev_err_ratelimited(dep->dwc->dev,
+				"end transfer failed: %d\n", ret);
+
 	dep->resource_index = 0;
 
 	if (!interrupt) {
@@ -4140,6 +4144,10 @@ static void dwc3_clear_stall_all_ep(struct dwc3 *dwc)
 		dep->flags &= ~DWC3_EP_STALL;
 
 		ret = dwc3_send_clear_stall_ep_cmd(dep);
+		if (ret)
+			dev_err_ratelimited(dwc->dev,
+				"failed to clear STALL on %s\n", dep->name);
+
 		WARN_ON_ONCE(ret);
 	}
 }
