025a1cf50da69e0ba067fb7e0a540f83d8d4d165
Michael Trimarchi <michael@amarulasolutions.com>
2026-05-13 14:21:34 +0200

    drm/panthor: Add flag to map GEM object Write-Back Cacheable
    Will be used by the UMD to optimize CPU accesses to buffers
    that are frequently read by the CPU, or on which the access
    pattern makes non-cacheable mappings inefficient.
    
    Mapping buffers CPU-cached implies taking care of the CPU
    cache maintenance in the UMD, unless the GPU is IO coherent.
    
    v2:
    - Add more to the commit message
    - Tweak the doc
    - Make sure we sync the section of the BO pointing to the CS
      syncobj before we read its seqno
    
    v3:
    - Fix formatting/spelling issues
    
    v4:
    - Add Steve's R-b
    
    v5:
    - Drop Steve's R-b (changes in the ioctl semantics requiring
      new review)
    
    v6:
    - Fix the uAPI doc
    - Fix inverted logic in some comment
    
    v7:
    - No changes
    
    v8:
    - Collect R-b
    
    Change-Id: I46b92c37c3f63b99aa587dc32745eec4f558b82c
    Signed-off-by: Loïc Molinari <loic.molinari@collabora.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Link: https://patch.msgid.link/20251208100841.730527-7-boris.brezillon@collabora.com
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- include/uapi/drm/panthor_drm.h
+++ include/uapi/drm/panthor_drm.h
@@ -365,11 +365,14 @@ struct drm_panthor_gpu_info {
 	/** @tiler_present: Bitmask encoding the tiler units exposed by the GPU. */
 	__u64 tiler_present;
 
-	/* @core_features: Used to discriminate core variants when they exist. */
+	/** @core_features: Used to discriminate core variants when they exist. */
 	__u32 core_features;
 
-	/* @pad: MBZ. */
+	/** @pad: MBZ. */
 	__u32 pad;
+
+	/** @gpu_features: Bitmask describing supported GPU-wide features */
+	__u64 gpu_features;
 };
 
 /**
@@ -678,6 +681,15 @@ struct drm_panthor_vm_get_state {
 enum drm_panthor_bo_flags {
 	/** @DRM_PANTHOR_BO_NO_MMAP: The buffer object will never be CPU-mapped in userspace. */
 	DRM_PANTHOR_BO_NO_MMAP = (1 << 0),
+
+	/**
+	 * @DRM_PANTHOR_BO_WB_MMAP: Force "Write-Back Cacheable" CPU mapping.
+	 *
+	 * CPU map the buffer object in userspace by forcing the "Write-Back
+	 * Cacheable" cacheability attribute. The mapping otherwise uses the
+	 * "Non-Cacheable" attribute if the GPU is not IO coherent.
+	 */
+	DRM_PANTHOR_BO_WB_MMAP = (1 << 1),
 };
 
 /**
